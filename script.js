03452384302c3a22f5d8493ef76d22024870cf87
Debmalya Sur <surdebmalya2001@gmail.com>
2021-03-31 18:34:21 +0530

    cv field added and styled

--- script.js
+++ script.js
@@ -34,4 +34,26 @@ const navSlide = () => {
 
 }
 
+// Back to top button
+$(window).scroll(function () {
+	if ($(this).scrollTop() > 100) {
+		$('.back-to-top').fadeIn('slow');
+	} else {
+		$('.back-to-top').fadeOut('slow');
+	}
+});
+
+$('.back-to-top').click(function () {
+	$('html, body').animate({
+		scrollTop: 0
+	}, 1500, 'easeInOutExpo');
+	return false;
+});
+
+// Open PDF
+function openTab(th) {
+	window.open(th.name, '_blank');
+}
+
+
 window.onload = () => navSlide();
